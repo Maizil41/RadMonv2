7091c09f7b75b580b4cd51a719f5c9957ea5bc44
Maizil <142029402+Maizil41@users.noreply.github.com>
2024-11-27 07:24:31 +0700

    MikroTik disconnect support

--- backend/disconnect.php
+++ backend/disconnect.php
@@ -13,15 +13,17 @@ require_once '../config/mysqli_db.php';
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $user = mysqli_real_escape_string($conn, $_GET['id']);
 
-    $sql = "SELECT acctsessionid, framedipaddress FROM radacct WHERE username = '$user' AND acctstoptime IS NULL";
+    $sql = "SELECT acctsessionid, nasipaddress, framedipaddress FROM radacct WHERE username = '$user' AND acctstoptime IS NULL";
     $result = mysqli_query($conn, $sql);
 
     if ($result && mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $acctid = $row['acctsessionid'];
             $framedipaddress = $row['framedipaddress'];
+            $nasipaddress = ($row['nasipaddress'] === '10.10.10.1') ? '127.0.0.1' : $row['nasipaddress'];
+            $nasipaddress = escapeshellarg($nasipaddress);
 
-            $command = 'echo \'User-Name="' . $user . '",Acct-Session-Id="' . $acctid . '",Framed-IP-Address="' . $framedipaddress . '"\' | radclient -c 1 -n 3 -r 3 -t 3 -x 127.0.0.1:3799 disconnect testing123 2>&1';
+            $command = 'echo \'User-Name="' . $user . '",Acct-Session-Id="' . $acctid . '",Framed-IP-Address="' . $framedipaddress . '"\' | radclient -c 1 -n 3 -r 3 -t 3 -x ' . $nasipaddress . ':3799 disconnect testing123 2>&1';
             shell_exec($command);
         }
     }
