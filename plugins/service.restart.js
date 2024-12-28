aebdaeec2eb5e6e274f6f06905eb16c6fe387ffc
Maizil <142029402+Maizil41@users.noreply.github.com>
2024-12-28 05:50:15 -0800

    Update service.restart.js

--- plugins/service.restart.js
+++ plugins/service.restart.js
@@ -2,4 +2,4 @@
  * Radius Monitor Service Restart
  * Author : Maizil <https://github.com/maizil41>
  */
-function confirmRestart(serviceName){if(confirm(`Apakah Anda yakin ingin merestart ${serviceName}?`)){fetch(`../backend/service.php?service=${serviceName}`).then(response=>response.text()).then(data=>{location.reload()}).catch(error=>{alert("Terjadi kesalahan: "+error)})}}
+function confirmRestart(serviceName, displayName){if(confirm(`Apakah Anda yakin ingin merestart ${displayName}?`)){fetch(`../backend/service.php?service=${serviceName}`).then(response=>response.text()).then(data=>{location.reload()}).catch(error=>{alert("Terjadi kesalahan: "+error)})}}
