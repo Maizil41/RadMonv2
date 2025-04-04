695f558f31263ec2df9931f924eb4e07dcfad042
Maizil <142029402+Maizil41@users.noreply.github.com>
2025-04-04 12:03:35 +0700

    Update pdo_db.php

--- config/pdo_db.php
+++ config/pdo_db.php
@@ -3,7 +3,7 @@ function get_db_connection() {
 
     require_once 'db_config.php';
 
-    $host = $db_config['hostname'];
+    $host = $db_config['servername'];
     $db = $db_config['dbname'];
     $user = $db_config['username'];
     $pass = $db_config['password'];
